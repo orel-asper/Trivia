7c25549a6447c07e7938677b415a16ddff4de2ac
Orel A <proorel@gmail.com>
2020-10-18 18:03:50 +0300

    initial commit

---
+++ precache-manifest.2ea86213daabb591229941cae660bc77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e62f1fde7049f68651d34fd4ff1aea7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9cee31d140cb2ee88292",
+    "url": "./static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9cee31d140cb2ee88292",
+    "url": "./static/js/2.250feb76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.250feb76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ebea0dc4cff97c23273",
+    "url": "./static/js/main.06c4c6c8.chunk.js"
+  },
+  {
+    "revision": "278fc398243823abd747",
+    "url": "./static/js/runtime-main.5efe9ea6.js"
+  }
+]);
